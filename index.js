85a396fb1622c87aa99a097093eba3bac2145142
Sean Jacke <sean.jacke@learnship.com>
2015-11-06 18:17:44 +0100

    switches from express to koa

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-var express = require('express');
-var app = express();
+var app = require('koa')();
+var router = require('koa-router')();
+var sendfile = require('koa-sendfile');
 var server;
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/static/html/start.html');
+router.get('/', function *(next) {
+    console.log(__dirname + '/static/html/start.html');
+
+    yield* sendfile.call(this, __dirname + '/static/html/start.html');
+    
+    if (!this.status) {
+        this.throw(404);
+    }
 });
+
+app
+.use(router.routes())
+.use(router.allowedMethods());
 
 server = app.listen(process.env.PORT || 3000, function () {
     console.log('Server running on port ' + server.address().port);
